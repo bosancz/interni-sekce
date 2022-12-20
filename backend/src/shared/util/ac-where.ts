2977a74e89afdd9668a3febe995fcfc113a84ca2
Kopec <kopec@bosan.cz>
2022-12-20 18:05:14 +0000

    style: improve acWhere function

--- backend/src/shared/util/ac-where.ts
+++ backend/src/shared/util/ac-where.ts
@@ -1,11 +1,23 @@
+import { ForbiddenException, InternalServerErrorException } from "@nestjs/common";
 import { Request } from "express";
 import { AcEntity } from "src/access-control/schema/ac-entity";
 import { AccessControlService } from "src/access-control/services/access-control.service";
-import { ObjectLiteral } from "typeorm";
+import { Brackets, ObjectLiteral } from "typeorm";
 import { QueryFilter } from "../schema/query-filter";
-import { permissionsToWhere } from "./permissions-to-where";
 
 export function acWhere<D extends ObjectLiteral>(acl: AccessControlService, entity: AcEntity<string, D, QueryFilter>, req: Request) {
   const permissions = acl.getPermissions(entity, req);
-  return permissionsToWhere(permissions, req);
+
+  if (permissions.some((p) => p === true)) return {};
+  if (permissions.every((p) => p === false)) throw new ForbiddenException();
+
+  return new Brackets((qb) => {
+    permissions.reduce((acc, cur, i) => {
+      if (typeof cur !== "object" || !("where" in cur)) throw new InternalServerErrorException("Invalid value in permissions.");
+
+      const b = new Brackets((qb) => cur.where(qb, req));
+
+      return qb.orWhere(b);
+    }, qb);
+  });
 }
