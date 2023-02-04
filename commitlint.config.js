e2dd74bb7e46468c539bb211c56f86043c572802
Martin Kopeček <martin.kopecek@smallhill.cz>
2023-02-04 11:13:25 +0100

    build: add "wip" to allowed commit types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const conventional = require("@commitlint/config-conventional");
+const types = [...conventional.rules["type-enum"][2], "wip"];
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", types],
+  },
+};
