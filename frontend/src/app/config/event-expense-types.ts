7c546aa702f3cb97bf4c91739fca0d95536b8f1f
Martin Kopeček <martin.kopecek@smallhill.cz>
2023-04-15 11:33:39 +0200

    feat: add expense types

--- frontend/src/app/config/event-expense-types.ts
+++ frontend/src/app/config/event-expense-types.ts
@@ -2,6 +2,8 @@ export enum EventExpenseTypeId {
   "Potraviny" = "Potraviny",
   "Doprava" = "Doprava",
   "Materiál" = "Materiál",
+  "Ubytování" = "Ubytování",
+  "Ostatní" = "Ostatní služby",
 }
 
 export interface EventExpenseType {
@@ -10,7 +12,9 @@ export interface EventExpenseType {
 }
 
 export const EventExpenseTypes: { [id in EventExpenseTypeId]: EventExpenseType } = {
-  "Potraviny": { title: "Potraviny", color: "primary" },
-  "Doprava": { title: "Doprava", color: "secondary" },
-  "Materiál": { title: "Materiál", color: "dark" },
+  Potraviny: { title: "Potraviny", color: "primary" },
+  Doprava: { title: "Doprava", color: "secondary" },
+  Materiál: { title: "Materiál", color: "dark" },
+  Ubytování: { title: "Ubytování", color: "tertiary" },
+  "Ostatní služby": { title: "Ostatní služby", color: "secondary" },
 };
