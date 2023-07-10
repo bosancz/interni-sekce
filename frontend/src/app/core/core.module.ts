e0675d7177a60477bf44f69c75c997d20712e4cf
Kopec <kopec@bosan.cz>
2023-07-10 00:28:58 +0000

    wip: loading component

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
 import { AdminMenuComponent } from "./components/admin-menu/admin-menu.component";
 import { AppLayoutComponent } from "./components/app-layout/app-layout.component";
+import { AppLoadingComponent } from "./components/app-loading/app-loading.component";
 import { DarkModeToggleComponent } from "./components/dark-mode-toggle/dark-mode-toggle.component";
 import { LoginComponent } from "./components/login/login.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
@@ -16,6 +17,7 @@ import { NotFoundComponent } from "./pages/not-found/not-found.component";
     DarkModeToggleComponent,
     SidebarComponent,
     AppLayoutComponent,
+    AppLoadingComponent,
   ],
   imports: [CommonModule, SharedModule],
   exports: [
@@ -25,6 +27,7 @@ import { NotFoundComponent } from "./pages/not-found/not-found.component";
     DarkModeToggleComponent,
     SidebarComponent,
     AppLayoutComponent,
+    AppLoadingComponent,
   ],
 })
 export class CoreModule {}
