6a16c84130128f461f66c27427cba58f4c3d62a1
Kopec <kopec@bosan.cz>
2023-07-27 10:42:54 +0000

    feat: member edit layout

--- frontend/src/app/modules/members/components/member-contacts/member-contacts.component.ts
+++ frontend/src/app/modules/members/components/member-contacts/member-contacts.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ToastService } from "src/app/services/toast.service";
 
 @Component({
-  selector: 'bo-member-contacts',
-  templateUrl: './member-contacts.component.html',
-  styleUrls: ['./member-contacts.component.scss']
+  selector: "bo-member-contacts",
+  templateUrl: "./member-contacts.component.html",
+  styleUrls: ["./member-contacts.component.scss"],
 })
 export class MemberContactsComponent {
+  contacts = [
+    { label: "Matka", type: "mobile", value: "+420 123 456 789" },
+    { label: "Otec", type: "mobile", value: "+420 123 456 789" },
+    { label: "Bratr", type: "email", value: "bratr@email.cz" },
+  ];
 
+  constructor(private toastService: ToastService) {}
+
+  async copyContact(contact: string) {
+    await navigator.clipboard.writeText(contact);
+    await this.toastService.toast("Kontakt byl zkopírován do schránky");
+  }
 }
