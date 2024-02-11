bf1cf3b020669c3f3d73a268a66e3f2bba791a80
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-02-11 00:21:33 +0000

    fix: migraton

--- backend/src/migrations/1707585178877-expenses-receipt-id.ts
+++ backend/src/migrations/1707585178877-expenses-receipt-id.ts
@@ -4,7 +4,8 @@ export class ExpensesReceiptId1707585178877 implements MigrationInterface {
   name = "ExpensesReceiptId1707585178877";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "events_expenses" ADD "receipt_number" character varying NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "events_expenses" ADD "receipt_number" character varying NOT NULL DEFAULT ''`);
+    await queryRunner.query(`ALTER TABLE "events_expenses" ALTER COLUMN "receipt_number" DROP DEFAULT`);
     await queryRunner.query(`CREATE INDEX "IDX_a6aeb17233427f9165e9661448" ON "events_groups" ("event_id") `);
     await queryRunner.query(`CREATE INDEX "IDX_0593cf7f0f7560db1b5ad7a6f3" ON "events_groups" ("group_id") `);
   }
