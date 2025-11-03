19cf72f2a6a72f52412f5c2eddcd1202d06bc6c0
Martin Kopeček <github@smallhill.cz>
2025-11-03 11:29:09 +0000

    wip

--- backend/src/api/events/controllers/cpv-events.controller.ts
+++ backend/src/api/events/controllers/cpv-events.controller.ts
@@ -13,8 +13,41 @@ export class CPVEventsController {
 	@AcLinks(CPVEventsListPermission)
 	@ApiResponse({ status: 200, type: WithLinks(CPVEventResponse), isArray: true })
 	async getCPVEvents(@Req() req: Request): Promise<CPVEventResponse[]> {
-		CPVEventsListPermission.canOrThrow(req);
-		// TODO: load events from raft.cz
+		// CPVEventsListPermission.canOrThrow(req);
+		// const events = [];
+		//   let i = 1;
+
+		//   while (i <= 10) {
+		// 	try {
+		// 	  var padlerResponse = await axios(
+		// 		`https://www.padler.cz/kalendar-akci/mesic/vse/rok/vse/lokalita/vse/typ-akce/1-2-5-9-17/strana/${i}/`,
+		// 		{ responseType: "text" }
+		// 	  );
+
+		// 	  const $ = cheerio.load(padlerResponse.data, { xmlMode: true });
+
+		// 	  $("a.trailLink").each((i, el) => {
+		// 		const name = $(el).find(".trailLink__title").text().trim();
+		// 		const link = $(el).attr("href");
+		// 		const dateFrom = $(el).find(".trailLink__date").attr("datetime");
+
+		// 		events.push({
+		// 		  source: "padler",
+		// 		  name,
+		// 		  dateFrom,
+		// 		  dateTill: dateFrom,
+		// 		  link,
+		// 		});
+		// 	  });
+
+		// 	  i++;
+		// 	} catch (err) {
+		// 	  break;
+		// 	}
+		//   }
+
+		//   res.json(events);
+
 		return [];
 	}
 }
