554b3206e310fdcbdc927586f8f764b05b29909d
Martin Kopeček <martin.kopecek@smallhill.cz>
2026-01-06 17:43:51 +0000

    chore(frontend): angular 21

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,12 @@ import localeCs from "@angular/common/locales/cs";
 import { enableProdMode } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { appConfig } from "./app/app.config";
 import { environment } from "./environments/environment";
 import { Logger } from "./logger";
 
 const logger = new Logger("MAIN");
+
 logger.log(`Starting application...`);
 logger.debug(`Environment: ${environment.production ? "production" : "development"}`);
 
@@ -16,6 +18,6 @@ if (environment.production) {
 
 registerLocaleData(localeCs);
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent, appConfig)
 	.then(() => logger.log(`Application started`))
 	.catch((err) => logger.error(`Application failed to start`, err));
