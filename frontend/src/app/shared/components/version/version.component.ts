eb791a31f56d59db043c26120cfc1b16801d60b9
Martin Kopeček <martin.kopecek@smallhill.cz>
2026-01-06 11:54:07 +0000

    feat(frontend): update notification

--- frontend/src/app/shared/components/version/version.component.ts
+++ frontend/src/app/shared/components/version/version.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { toSignal } from "@angular/core/rxjs-interop";
+import { SwUpdate } from "@angular/service-worker";
 import { map } from "rxjs";
 import { ApiService } from "src/app/services/api.service";
+import { Logger } from "src/logger";
 
 @Component({
 	selector: "bo-version",
@@ -10,7 +12,38 @@ import { ApiService } from "src/app/services/api.service";
 	styleUrl: "./version.component.scss",
 })
 export class VersionComponent {
-	version = toSignal(this.api.info.pipe(map((info) => info.version)));
+	private readonly logger = new Logger("VersionComponent");
 
-	constructor(private readonly api: ApiService) {}
+	version = toSignal(this.api.info.pipe(map((info) => info.version)));
+	updateStatus = signal<"checking" | "available" | "unavailable" | "error">("available");
+
+	constructor(
+		private readonly api: ApiService,
+		private readonly swUpdate: SwUpdate,
+	) {
+		this.checkForUpdates();
+	}
+
+	private async checkForUpdates(): Promise<void> {
+		if (this.swUpdate.isEnabled) {
+			this.updateStatus.set("checking");
+			this.logger.debug("Checking for updates...");
+
+			try {
+				const updateAvailable = await this.swUpdate.checkForUpdate();
+				this.updateStatus.set(updateAvailable ? "available" : "unavailable");
+
+				this.logger.log("Update available:", updateAvailable);
+			} catch {
+				this.updateStatus.set("error");
+				this.logger.error("Error while checking for updates");
+			} finally {
+				this.logger.debug("Update check completed");
+			}
+		}
+	}
+
+	doUpdate(): void {
+		this.swUpdate.activateUpdate().then(() => document.location.reload());
+	}
 }
