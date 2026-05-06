1a3f203658cd6d2654ade2bd1dc75e8979138095
Martin Kopeček <martin.kopecek@smallhill.cz>
2026-05-06 14:26:36 +0000

    chore: update Angular dependencies to version 21.2.x and remove unused loading animation logic

--- frontend/src/app/core/components/app-loading/app-loading.component.ts
+++ frontend/src/app/core/components/app-loading/app-loading.component.ts
@@ -1,12 +1,8 @@
 import { Component } from "@angular/core";
-import { toSignal } from "@angular/core/rxjs-interop";
-import { map, timer } from "rxjs";
 
 @Component({
 	selector: "bo-app-loading",
 	templateUrl: "./app-loading.component.html",
 	styleUrls: ["./app-loading.component.scss"],
 })
-export class AppLoadingComponent {
-	ellipsis = toSignal(timer(0, 1000).pipe(map((i) => ".".repeat(i%4))));
-}
+export class AppLoadingComponent {}
