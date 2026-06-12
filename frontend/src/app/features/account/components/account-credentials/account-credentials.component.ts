81dfa469a5ea5639e5abf3fa910f449e0cbb259c
Jiří Kubíček <jiri.kubic@email.cz>
2026-06-12 19:55:44 +0000

    fix loading of the info in my account

--- frontend/src/app/features/account/components/account-credentials/account-credentials.component.ts
+++ frontend/src/app/features/account/components/account-credentials/account-credentials.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
 import { AlertController, IonItem, IonLabel, IonList, IonText } from "@ionic/angular/standalone";
 import { ApiService } from "src/app/core/services/api.service";
 import { ToastService } from "src/app/core/services/toast.service";
-import { SDK } from "src/sdk";
+import { UserService } from "src/app/core/services/user.service";
 
 @Component({
 	selector: "bo-account-credentials",
@@ -11,29 +12,25 @@ import { SDK } from "src/sdk";
 
 	imports: [IonList, IonItem, IonLabel, IonText],
 })
-export class AccountCredentialsComponent implements OnInit {
-	user?: SDK.AccountResponseWithLinks;
+export class AccountCredentialsComponent {
+	user = toSignal(this.userService.user);
 
 	constructor(
 		private api: ApiService,
+		private userService: UserService,
 		private toastService: ToastService,
 		private alertController: AlertController,
 	) {}
 
-	ngOnInit() {
-		this.loadUser();
-	}
-
-	async loadUser() {
-		this.user = await this.api.AccountApi.getMe().then((res) => res.data);
-	}
-
 	async updateCredentials(credentials: { login: string; password: string }) {
-		if (!this.user) return;
+		const user = this.user();
+		if (!user) return;
 
-		await this.api.UsersApi.setUserPassword(this.user.id, credentials);
+		await this.api.UsersApi.setUserPassword(user.id, credentials);
 
 		this.toastService.toast("Uloženo.");
+
+		await this.userService.loadUser();
 	}
 
 	async openChangeCredentials() {
@@ -44,7 +41,7 @@ export class AccountCredentialsComponent implements OnInit {
 					name: "login",
 					type: "text",
 					placeholder: "Login: bilbo",
-					value: this.user?.login,
+					value: this.user()?.login,
 				},
 				{
 					name: "password",
