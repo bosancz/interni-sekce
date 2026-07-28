5b02c28d1dded57ded24c3ac2adbed863761b765
Martin Kopeček <martin.kopecek@smallhill.cz>
2026-07-28 09:12:21 +0000

    fix: fix unused imports

--- frontend/src/app/shared/components/member-item-detail/member-item-detail.component.ts
+++ frontend/src/app/shared/components/member-item-detail/member-item-detail.component.ts
@@ -1,13 +1,11 @@
 import { Component, input, OnInit } from "@angular/core";
 import { SDK } from "src/sdk";
-import { MemberPipe } from "../../pipes/member.pipe";
 
 @Component({
 	selector: "bo-member-item-detail",
 	templateUrl: "./member-item-detail.component.html",
 	styleUrls: ["./member-item-detail.component.scss"],
-
-	imports: [MemberPipe],
+	imports: [],
 })
 export class MemberItemDetailComponent implements OnInit {
 	member = input.required<SDK.MemberResponse>();
